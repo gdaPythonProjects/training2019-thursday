1a55d4819c25c2d64a916fd9a29c9390c7225bcf
Michal Zylowski <zylowskimichal@gmail.com>
2019-11-07 19:17:31 +0100

    modulo
    Signed-off-by: Michal Zylowski <zylowskimichal@gmail.com>

--- 1/modulo.py
+++ 1/modulo.py
@@ -1,1 +1,5 @@
-x = int(input("Podaj liczbę do sprawdzenia"))
+x = int(input("Podaj liczbę do sprawdzenia\n"))
+if x % 2 == 0:
+    print(x, "jest parzysta")
+else:
+    print(x, "NIE jest parzysta")
