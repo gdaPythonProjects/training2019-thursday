7ad99142ff3f64cb7b6c4b64093710e5bc5c9374
Michal Zylowski <zylowskimichal@gmail.com>
2019-11-07 19:09:05 +0100

    input
    Signed-off-by: Michal Zylowski <zylowskimichal@gmail.com>

---
+++ 1/input.py
@@ -0,0 +1,8 @@
+x = int(input("Podaj liczbę\n"))
+y = int(input("Podaj drugą liczbę\n"))
+if x == 5 and y == 2:
+    print("Brawo! Odgadłeś kombinację!")
+elif x == 5 or y == 2:
+    print("Jesteś blisko :)")
+else:
+    print("No niestety...")
